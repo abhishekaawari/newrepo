7930e3355d811d22554fa5c075f49ad4b811f6d0
abhishek <abhishek.aawari@borntechies.com>
2020-04-23 19:25:22 +0530

    product.php changed

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -2,33 +2,32 @@
 namespace Product\Collection\Controller\Index;
  use Magento\Framework\App\Action\Context;
 
-
-class Index extends \Magento\Framework\App\Action\Action
-{
-    protected $_pageFactory;
-
-	public function __construct(
-		\Magento\Framework\App\Action\Context $context,
-		\Magento\Framework\View\Result\PageFactory $pageFactory
-	)
+ class Index extends \Magento\Framework\App\Action\Action
 	{
-		$this->_pageFactory = $pageFactory;
-		return parent::__construct($context);
-	}
+	    protected $_pageFactory;
 
-	public function execute()
-	{	
+		public function __construct(
+			\Magento\Framework\App\Action\Context $context,
+			\Magento\Framework\View\Result\PageFactory $pageFactory
+		)
+		{
+			$this->_pageFactory = $pageFactory;
+			return parent::__construct($context);
+		}
 
-		// $post = $this->getRequest()->getParams('val2');
-		// print_r($post);
-		// if (array_key_exists('val2', $post)) {
-		//     echo $post['val2'];
-		// }		//die('hi');
+		public function execute()
+		{	
 
-		// print_r($this->request->getParams());
+			// $post = $this->getRequest()->getParams('val2');
+			// print_r($post);
+			// if (array_key_exists('val2', $post)) {
+			//     echo $post['val2'];
+			// }		//die('hi');
 
-		return $this->_pageFactory->create();
-	}
+			// print_r($this->request->getParams());
+
+			return $this->_pageFactory->create();
+		}
        
-}       
+	}       
 ?>
